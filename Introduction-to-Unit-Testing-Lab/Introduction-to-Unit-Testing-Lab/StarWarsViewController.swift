04e18f533aa69c9d34b4bac34d6745d5ca966659
shaniyaUwagboe <shaniyauwagboe@pursuit.org>
2019-12-02 16:36:57 -0500

    tableViews have their own view controller

---
+++ Introduction-to-Unit-Testing-Lab/Introduction-to-Unit-Testing-Lab/StarWarsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StarWarsViewController.swift
+//  Introduction-to-Unit-Testing-Lab
+//
+//  Created by World Domination a Brand on 12/2/19.
+//  Copyright © 2019 Pursuitful stuff. All rights reserved.
+//
+
+import UIKit
+
+class StarWarsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
